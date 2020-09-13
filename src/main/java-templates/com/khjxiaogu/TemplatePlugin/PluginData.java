5d3bc7017a0c01bb912f0325e294137687664ba2
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2020-09-14 00:47:41 +0800

    upload codes

---
+++ src/main/java-templates/com/khjxiaogu/TemplatePlugin/PluginData.java
@@ -0,0 +1,24 @@
+package com.khjxiaogu.TemplatePlugin;
+
+// TODO: Auto-generated Javadoc
+/**
+ * 自动替换的插件信息工具类
+ * @author khjxiaogu
+ * file: PluginData.java
+ * time: 2020年9月12日
+ */
+public class PluginData {
+	
+	/** The name.<br> 插件名称. */
+	public static String name="@project.artifactId@";
+	
+	/** The ver.<br> 插件版本. */
+	public static String version="@project.version@";
+	
+	/** The auth.<br> 插件作者. */
+	public static String author="khjxiaogu";
+	
+	/** The info.<br> 插件信息. */
+	public static String info="@project.description@";
+
+}
---
+++ src/main/java/com/khjxiaogu/TemplatePlugin/TemplatePlugin.java
@@ -0,0 +1,25 @@
+package com.khjxiaogu.TemplatePlugin;
+
+import com.khjxiaogu.TemplatePlugin.PluginData;
+
+import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
+import net.mamoe.mirai.console.plugin.jvm.SimpleJvmPluginDescription;
+
+// TODO: Auto-generated Javadoc
+/**
+ * 插件主类
+ * @author khjxiaogu
+ * file: MiraiSongPlugin.java
+ * time: 2020年8月26日
+ */
+public class TemplatePlugin extends JavaPlugin{
+	public TemplatePlugin() {
+		super(new SimpleJvmPluginDescription(PluginData.name,PluginData.version,PluginData.author,PluginData.info));
+	}
+	@Override
+	public void onEnable() {
+		
+		getLogger().info("插件加载完毕!");
+	}
+
+}
